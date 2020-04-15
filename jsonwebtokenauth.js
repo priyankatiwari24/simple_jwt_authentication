348e2aee09d936c070d64b362e1209e9ab5715a3
priyanka_tiwari <priyanka.tiwari@accionlabs.com>
2020-04-15 13:11:07 +0530

    jwt for login and post api

---
+++ jsonwebtokenauth.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Welcome to the API'
+  });
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {  
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  console.log(req.body);
+  // Mock user
+  const user = {
+    id: req.body.id, 
+    username: req.body.username,
+    email: req.body.email
+  }
+
+  jwt.sign({user}, 'secretkey', { expiresIn: '7d' }, (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    console.log(req.token);
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+
+}
+
+app.listen(5000, () => console.log('Server started on port 5000'));
